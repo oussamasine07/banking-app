c6e287cbe1b3ffe14f0fee8c5619cf01ed12a8f3
oussamasine07 <sineoussama@gmail.com>
2024-12-24 15:27:01 +0100

    add CheckingAccount class

---
+++ src/CheckingAccount.java
@@ -0,0 +1,20 @@
+public class CheckingAccount {
+    private double bankCharges;
+
+    CheckingAccount () {}
+    CheckingAccount ( double bankCharges ) {
+        this.bankCharges = bankCharges;
+    }
+
+    public double getBankCharges () {
+        return this.bankCharges;
+    }
+
+    public void setBankCharges ( double bankCharges ) {
+        this.bankCharges = bankCharges;
+    }
+
+
+
+    
+}
